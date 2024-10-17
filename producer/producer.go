ca10eff79075355f41e89cb3b64b6e783bc08559
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-10-18 00:58:35 +0100

    changed port to default kafka port

--- producer/producer.go
+++ producer/producer.go
@@ -34,7 +34,7 @@ func ConnectProducer(brokersUrl []string) (sarama.SyncProducer, error) {
 }
 
 func PushCommentToQueue(topic string, message []byte) error {
-	brokersUrl := []string{"localhost: 8080"}
+	brokersUrl := []string{"localhost: 9092"}
 	producer, err := ConnectProducer(brokersUrl)
 	if err != nil {
 		return err
