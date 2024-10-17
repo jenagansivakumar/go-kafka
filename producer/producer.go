f1f6846bb25ab21752ba1e7e2d04c0b426412e00
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-10-17 23:15:26 +0100

    create comment underway

--- producer/producer.go
+++ producer/producer.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"log"
+)
+
 type comment struct {
 	Text string `form: "text" json: "text"`
 }
@@ -7,4 +11,16 @@ type comment struct {
 func main() {
 	app := fiber.New()
 	api := app.Group("/api/v1")
+	api.Post("/comment", createComment)
+	app.Listen(":3000")
+}
+
+func createComment(c fiber.Ctx) error {
+	cmt := new(Comment)
+	if err := c.BodyParser(cmt); err != nil {
+		log.Println(err)
+		c.Status(400).JSON(&fiber.Map{
+			"success": false,
+		})
+	}
 }
