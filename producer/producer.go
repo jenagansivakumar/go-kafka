d98f631b7e269901d032434f8ff7171c76107e4d
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-10-18 00:56:13 +0100

    fixed * error in fiber

--- producer/producer.go
+++ producer/producer.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 
 	"github.com/IBM/sarama"
@@ -43,10 +44,15 @@ func PushCommentToQueue(topic string, message []byte) error {
 		Topic: topic,
 		Value: sarama.StringEncoder(message),
 	}
-	producer.SendMessage(msg)
+	partition, offset, err := producer.SendMessage(msg)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Message is stored in topic (%s)/partition (%d)/offset(%d)\n", topic, partition, offset)
+	return nil
 }
 
-func createComment(c fiber.Ctx) error {
+func createComment(c *fiber.Ctx) error {
 	cmt := new(comment)
 	if err := c.BodyParser(cmt); err != nil {
 		log.Println(err)
