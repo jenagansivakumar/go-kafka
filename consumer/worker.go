a3c12935616048ef73f60b28468c03a37951db15
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-10-18 02:22:03 +0100

    created worker

--- consumer/worker.go
+++ consumer/worker.go
@@ -56,4 +56,8 @@ func connectConsumer(brokersUrl [] string )(sarama.Consumer, error){
 	config := sarama.NewConfig()
 	config.Consumer.Return.Errors = true 
 	conn, err := sarama.NewConsumer(brokersUrl, config)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
 }
