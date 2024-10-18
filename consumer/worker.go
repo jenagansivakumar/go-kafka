b11999e589c9317dc224dffa99274a888b312daf
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-10-18 01:42:35 +0100

    creating go func

--- consumer/worker.go
+++ consumer/worker.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"structs"
 	"syscall"
 )
 
@@ -23,4 +24,18 @@ func main() {
 	fmt.Println("Consumer started")
 	sigChain := make(chan os.Signal, 1)
 	signal.Notify(sigChain, syscall.SIGINT, syscall.SIGTERM)
+
+	msgCount := 0
+
+	doneCh := make(chan struct{})
+
+	go func(){
+		for {
+			select {
+			case err := <-consumer.Error():
+				fmt.Printf(err)
+			}
+
+		}
+	}
 }
